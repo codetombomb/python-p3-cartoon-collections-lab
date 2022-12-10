2b99d88aba28e0f4876ac150a61236b0e20c9def
Tom Tobar <cocdetombomb@gmail.com>
2022-12-10 00:31:46 -0600

    complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list):
+    for idx, d in enumerate(list):
+        print(f"{idx + 1}. {d}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    return [f"{name.capitalize()}!" for name in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    for word in list:
+        if len(word) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    cheeses = ["cheddar", "gouda","camembert"]
+    for cheese in list:
+        if cheese in cheeses:
+            return cheese
+
